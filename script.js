a7cbc16849edd420f82372b7ae435ed992c1a145
ohzuma <ozumanology@gmail.com>
2024-03-16 04:48:18 -0700

    shorten url

--- script.js
+++ script.js
@@ -8,54 +8,8 @@ nav_mobile_btn.addEventListener("click", () => {
   mobile_menu.classList.toggle("active");
 });
 
-// async function ShortUrl(url, data) {
-//   const response = await fetch(url, {
-//     method: "POST",
-//     mode: "no-cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     // credentials: "same-origin",
-//     SameSite: "none",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Origin": "*",
-//     },
-//     body: JSON.stringify(data),
-//   });
-//   return response
-// }
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   try {
-//     if (form_input.value === "") {
-//       throw "Please provide a url";
-//     }
-//     const url = new URL(form_input.value);
-//     // console.log(url);
-//     ShortUrl("https://cleanuri.com/api/v1/shorten", `url=${url.href}`).then(
-//       (res) => {
-//         console.log(res);
-//       }
-//     );
-//   } catch (error) {
-//     error_text.textContent = error;
-//   }
-// });
-
 async function ShortUrl(url, data) {
-  const response = await fetch(url, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data: data }), // Ensure data is sent as an object
-  });
+  const response = await axios.post(url, { url: data });
   return response;
 }
 
@@ -67,12 +21,12 @@ form.addEventListener("submit", async (e) => {
       throw "Please provide a URL";
     }
     const url = new URL(form_input.value);
-    const response = await ShortUrl(
+    await ShortUrl(
       "https://cleanuri.com/api/v1/shorten",
-      url.href
-    );
-    const data = await response.json();
-    console.log(data);
+      `url=${url.href}`
+    ).then((res) => {
+      console.log(res);
+    });
   } catch (error) {
     error_text.textContent = error;
   }
